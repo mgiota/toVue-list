cb5de0c0e3541418e81adb75ede50266169c7db7
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-04 20:11:23 +0200

    add in script declaration of todos arr, currentTodo and addTodo methods plus template to render the todo list

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <div>
+    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 
 <script>
-
+  export default {
+    data() {
+      return {
+        todos: [],
+        currentTodo: ''
+      };
+    },
+    methods: {
+      addTodo() {
+        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+        this.currentTodo = '';
+      }
+    }
+  };
 </script>
 
 
