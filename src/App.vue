0558d7ca982dae57f9d23a13a33bb41ae9664f25
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-05 19:59:25 +0200

    add removeTodo and basic/temporary stylings

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="container">
+    <h1>Todo List</h1>
     <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
     <ul class="todos">
       <li v-for="todo in todos" :key="todo.id">
+        <button @click="removeTodo">X</button>
         {{ todo.label }}
       </li>
     </ul>
@@ -22,6 +24,10 @@
       addTodo() {
         this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
         this.currentTodo = '';
+      },
+      removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
       }
     }
   };
@@ -29,5 +35,33 @@
 
 
 <style>
+  body{
+    background-color: #5C6BC0;
+    padding: 3%;
+  }
 
+  .container{
+    background-color: white;
+    padding: 1%;
+    text-align: center;
+    width: 60%;
+    margin: 0 auto;
+  }
+
+  input{
+    padding: 10px;
+    margin: 0 auto;
+    outline: none
+  }
+
+  ul{
+    padding: 0;
+  }
+
+  li{
+    list-style: none;
+    border: solid black 1px;
+    margin: 10px auto;
+    padding: 10px;
+  }
 </style>
