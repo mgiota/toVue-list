2aa7f834adc40917f4945d341a15d79a178410ac
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-05 19:35:49 +0200

    install vue-material and add it in index.html and main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(MdField);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
