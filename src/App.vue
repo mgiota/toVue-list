5a2f5a64378d797c9911aca86b8e58741db45e0b
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-06 17:23:11 +0200

    add complete done and edit todos and using Vue custom tags instead of HTML

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="container">
-    <h1>Todo List</h1>
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        <button @click="removeTodo">X</button>
-        {{ todo.label }}
-      </li>
-    </ul>
+    <h1 class="title">Todo List</h1>
+
+    <md-field>
+      <md-button @click="addTodo()" class="md-icon-button addItem">
+        <md-icon>add</md-icon>
+      </md-button>
+      <md-input class="add" v-model="currentTodo" @keyup.enter="addTodo()" placeholder="Add a todo..."></md-input>
+    </md-field>
+
+    <md-list class="todos">
+      <md-list-item v-for="(todo, index) in todos" :key="todo.id" @dblclick="editTodo(index)" class="todo" :class="{completed: todo.completed}">
+
+        <md-checkbox v-model="todo.completed" value="todo.completed" @change="completeTodo(todo)">
+          <label v-if="!todo.edit">{{ todo.label }}</label>
+          <input class="edit" type="text" v-model="todo.label" v-if="todo.edit" v-focus @blur="doneEdit(index)" @keyup.enter="doneEdit(index)">
+        </md-checkbox>
+
+        <md-button class="remove" @click="removeTodo(todo)">X</md-button>
+      </md-list-item>
+    </md-list>
+
+    <footer v-show="todos.length > 0">
+      <md-button class="clear" @click="removeCompleted()">Clear completed</md-button>
+    </footer>
   </div>
 </template>
 
 
 <script>
+  const focus = {
+    inserted(el) {el.focus()}
+  };
+
   export default {
     data() {
       return {
@@ -22,14 +42,36 @@
     },
     methods: {
       addTodo() {
-        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-        this.currentTodo = '';
+        if (this.currentTodo !== "") {
+          this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false, edit: false
+          });
+        }
+        this.currentTodo = "";
+      },
+      completeTodo(todo) {
+        if (todo.completed === true) {
+          todo.completed = false;
+        }
+        if (todo.completed === false) {
+          todo.completed = true;
+        }
+      },
+      editTodo(index) {
+        this.todos[index].edit = true;
+      },
+      doneEdit(index) {
+        this.todos[index].edit = false;
       },
       removeTodo(todo) {
-      var index = this.todos.indexOf(todo);
-      this.todos.splice(index, 1);
+        var index = this.todos.indexOf(todo);
+        this.todos.splice(index, 1);
+      },
+      removeCompleted() {
+        this.todos = this.todos.filter(function(todo) {
+          return !todo.completed;
+        });
       }
-    }
+    }//methods
   };
 </script>
 
@@ -38,6 +80,7 @@
   body{
     background-color: #5C6BC0;
     padding: 3%;
+    height: 100%;
   }
 
   .container{
@@ -64,4 +107,15 @@
     margin: 10px auto;
     padding: 10px;
   }
+
+  footer {
+  width: 120px;
+  display: flex;
+  padding: 5px;
+  border-radius: 3px;
+  background-color: #1ea896
+  }
+
+  
+
 </style>
